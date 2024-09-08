7f1244280e9a12edd8e3268463e812cd851d5e79
azisssss <aziswh7@gmail.com>
2024-09-08 23:31:46 +0700

    storage hit fix

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -61,7 +61,7 @@
           <div class="row g-5">
             <div class="col-xl-6 wow fadeInLeft" data-wow-delay="0.2s">
                 <div class="about-img rounded h-100">
-                    <img src="{{ asset('storage/public' . $item->image) }}" class="img-fluid rounded h-100 w-100" style="object-fit: cover;" alt="{{ $item->imgtitle ?? 'EMPTY' }}">
+                    <img src="{{ asset('storage/public/' . $item->image) }}" class="img-fluid rounded h-100 w-100" style="object-fit: cover;" alt="{{ $item->imgtitle ?? 'EMPTY' }}">
                     <div class="about-exp"><span>{{ $item->imgtitle ?? 'EMPTY' }}</span></div>
                 </div>
             </div>
